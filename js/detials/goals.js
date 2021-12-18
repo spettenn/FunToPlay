0047ffde3f34cadcc1a9afe3da9a6725a78eeae5
spettenn <69841778+spettenn@users.noreply.github.com>
2021-12-18 14:33:10 +0100

    updates

--- js/detials/goals.js
+++ js/detials/goals.js
@@ -17,19 +17,18 @@ async function getGoals() {
 		console.log(response);
 		const jsonResults = await response.json();
 		console.log({ jsonResults });
-		const info = object.jsonResults;
 
 		document.querySelector('.details__wrapper').innerHTML += `
 <div class="detials__content">
 <div class="detials__img__container">
-<img src="${info.images}" />
+<img src="${jsonResults.images}" />
 </div>
 <div class="detials__card__title">
-<div class="detials__info__container">
-<h2>${info.name}</h2>
-<p>${info.description}</p>
-<p>${info.price}</p>
 </div>
+<h2>${jsonResults.name}</h2>
+<div class="detials__info__container">
+<p>${jsonResults.description}</p>
+<p>${jsonResults.price}</p>
 </div>
 </div>
 `;
